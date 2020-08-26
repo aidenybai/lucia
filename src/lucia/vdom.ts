d45a37013a827a3f1e9cbf34dbc35806a2258799
abai <mail.contraband@gmail.com>
2020-08-25 20:45:45 -0700

    feat(vdom): add prop methods

--- src/lucia/vdom.ts
+++ src/lucia/vdom.ts
@@ -1,60 +1,61 @@
 export default class VDom {
-  el: any;
+  $el: any;
   vdom: any;
 
-  constructor(el: any) {
-    this.el = el;
-    this.vdom = this.generate(this.el);
+  constructor($el: any) {
+    this.$el = $el;
+    this.vdom = this.compile(this.$el);
   }
 
-  generate(el: any): any {
+  compile($el: any): any {
     let children = [];
 
-    if (el instanceof Array) {
-      for (let i = 0; i < el.length; i++) {
-        if (el[i].children.length === 0) {
+    if ($el instanceof Array) {
+      // Pretty messy and inconcise, needs to be rewritten
+      for (let i = 0; i < $el.length; i++) {
+        if ($el[i].children.length === 0) {
           children.push(
-            this.h(el[i].tagName.toLowerCase(), this.getAttributesObject(el[i]), [el[i].innerHTML])
+            this.h($el[i].tagName.toLowerCase(), this.getPropsObject($el[i]), [$el[i].innerHTML])
           );
         } else {
           children.push(
             this.h(
-              el[i].tagName.toLowerCase(),
-              this.getAttributesObject(el),
-              this.generate([...el[i].children])
+              $el[i].tagName.toLowerCase(),
+              this.getPropsObject($el),
+              this.compile([...$el[i].children])
             )
           );
         }
       }
       return children;
     } else {
-      if (el.children.length === 0) {
-        return this.h(el.tagName.toLowerCase(), this.getAttributesObject(el), [el.innerHTML]);
+      if ($el.children.length === 0) {
+        return this.h($el.tagName.toLowerCase(), this.getPropsObject($el), [$el.innerHTML]);
       } else {
         return this.h(
-          el.tagName.toLowerCase(),
-          this.getAttributesObject(el),
-          this.generate([...el.children])
+          $el.tagName.toLowerCase(),
+          this.getPropsObject($el),
+          this.compile([...$el.children])
         );
       }
     }
   }
 
-  getAttributesObject(el: any) {
-    const attrObject: any = {};
-    if (el.attributes) {
-      for (let i = 0; i < el.attributes.length; i++) {
-        attrObject[el.attributes[i].name] = el.attributes[i].value;
+  getPropsObject($el: any) {
+    const propsObject: any = {};
+    if ($el.props) {
+      for (let i = 0; i < $el.props.length; i++) {
+        propsObject[$el.props[i].name] = $el.props[i].value;
       }
     }
 
-    return attrObject;
+    return propsObject;
   }
 
-  h(tagName: string, attributes: any, children: any) {
+  h(tagName: string, props: any, children: any) {
     return {
       tagName,
-      attributes: attributes || {},
+      props: props || {},
       children: children || [],
     };
   }
@@ -65,8 +66,8 @@ export default class VDom {
     }
     const $el = document.createElement(node.tagName);
     node.children.map(this.createElement.bind(this)).forEach($el.appendChild.bind($el));
-    Object.keys(node.attributes).map((key) => {
-      $el.setAttribute(key, node.attributes[key]);
+    Object.keys(node.props).map((key) => {
+      $el.setAttribute(key, node.props[key]);
     });
     return $el;
   }
@@ -76,8 +77,8 @@ export default class VDom {
       typeof node1 !== typeof node2 ||
       (typeof node1 === 'string' && node1 !== node2) ||
       node1.tagName !== node2.tagName ||
-      node1.attributes !== node2.attributes ||
-      node1.children !== node2.children 
+      node1.props !== node2.props ||
+      node1.children !== node2.children
     );
   }
 
@@ -88,13 +89,88 @@ export default class VDom {
       $parent.removeChild($parent.childNodes[index]);
     } else if (this.diffNodes(newNode, oldNode)) {
       $parent.replaceChild(this.createElement(newNode), $parent.childNodes[index]);
-    } else if (newNode.type) {
+    } else if (newNode.tagName) {
       const newLength = newNode.children.length;
       const oldLength = oldNode.children.length;
       for (let i = 0; i < newLength || i < oldLength; i++) {
         this.hydrate($parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
       }
     }
-    this.generate(this.el); // NEED CONCISE HYDRATION
+    this.compile($parent); // NEED CONCISE HYDRATION FOR PERF
+  }
+
+  setBooleanProp($target: any, name: any, value: any) {
+    if (value) {
+      $target.setAttribute(name, value);
+      $target[name] = true;
+    } else {
+      $target[name] = false;
+    }
+  }
+
+  removeBooleanProp($target: any, name: any) {
+    $target.removeAttribute(name);
+    $target[name] = false;
+  }
+
+  isEventProp(name: any) {
+    return /^on/.test(name);
+  }
+
+  extractEventName(name: any) {
+    return name.slice(2).toLowerCase();
+  }
+
+  isCustomProp(name: any) {
+    return this.isEventProp(name); // can add some type of directive handling here
+  }
+
+  setProp($target: any, name: any, value: any) {
+    if (this.isCustomProp(name)) {
+      return;
+    } else if (typeof value === 'boolean') {
+      this.setBooleanProp($target, name, value);
+    } else {
+      $target.setAttribute(name, value);
+    }
+  }
+
+  removeProp($target: any, name: any, value: any) {
+    if (this.isCustomProp(name)) {
+      return;
+    } else if (typeof value === 'boolean') {
+      this.removeBooleanProp($target, name);
+    } else {
+      $target.removeAttribute(name);
+    }
+  }
+
+  setProps($target: any, props: any) {
+    Object.keys(props).forEach((name) => {
+      this.setProp($target, name, props[name]);
+    });
+  }
+
+  updateProp($target: any, name: any, newVal: any, oldVal: any) {
+    if (!newVal) {
+      this.removeProp($target, name, oldVal);
+    } else if (!oldVal || newVal !== oldVal) {
+      this.setProp($target, name, newVal);
+    }
+  }
+
+  updateProps($target: any, newProps: any, oldProps: any = {}) {
+    const props = Object.assign({}, newProps, oldProps);
+    Object.keys(props).forEach((name) => {
+      this.updateProp($target, name, newProps[name], oldProps[name]);
+    });
+  }
+
+  addEventListeners($target: any, props: any) {
+    Object.keys(props).forEach((name) => {
+      if (this.isEventProp(name)) {
+        $target.addEventListener(this.extractEventName(name), props[name]);
+      }
+    });
   }
 }
