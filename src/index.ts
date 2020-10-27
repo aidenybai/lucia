407a7e54faae3a6d37da2d572f72cbfb7ecfe49e
abai <mail.contraband@gmail.com>
2020-10-27 11:58:14 -0700

    refactor(index): rename Lucia class to App for consistency

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import VDom from './vdom';
 
-export class Lucia extends VDom {
+export class App extends VDom {
   constructor(options: Record<string, unknown>) {
     super(options || {});
   }
 }
 
 export const createApp = (options: Record<string, unknown>) => {
-  return new Lucia(options);
+  return new App(options);
 };
 
-export const links: Record<string, Lucia> = {};
+export const links: Record<string, App> = {};
 
 document.addEventListener('DOMContentLoaded', () => {
   const elements = Array.from(document.querySelectorAll('[l-use]'));
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   for (const el of elements) {
     const options = el.getAttribute('l-use');
     if (options === null) return;
-    
+
     const app = createApp(new Function(`return (${options})`)());
     const link = el.getAttribute('l-link');
     if (link) links[link] = app;
