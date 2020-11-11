6f3e983035319328ae32ec77d6830f4666f0d296
Aiden Bai <mail.contraband@gmail.com>
2020-11-11 14:24:36 -0800

    feat(vdom): remove with
    when state is referenced in directives, they must be appended with `this.`

--- src/helpers/compute.ts
+++ src/helpers/compute.ts
@@ -1,5 +1,9 @@
-const computeProperties = (expression: string, view: unknown = {}, returnable: boolean = true): any => {
-  return new Function(`with(this) {${returnable ? `return ${expression}` : expression}}`).bind(view)();
+const computeProperties = (
+  expression: string,
+  view: unknown = {},
+  returnable: boolean = true
+): any => {
+  return new Function(`${returnable ? `return ${expression}` : expression}`).bind(view)();
 };
 
 export default computeProperties;
