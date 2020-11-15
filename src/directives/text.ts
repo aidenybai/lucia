b01ee88dbb1a2396fdd8ca05323b21803d70e3e2
Aiden Bai <mail.contraband@gmail.com>
2020-11-14 20:11:29 -0800

    style: cleanup

--- src/directives/text.ts
+++ src/directives/text.ts
@@ -4,5 +4,5 @@ import { args } from './args';
 export const textDirective = ({ el, value, view }: args) => {
   const out = compute(value, view);
 
-  el.textContent = out ?? value; 
+  el.textContent = out ?? value;
 };
