51ce5a67f387e727e192e1ba2243f341917da0a8
Aiden Bai <mail.contraband@gmail.com>
2020-11-23 13:19:23 -0800

    feat: add support for selector parsing

--- src/vdom/h.ts
+++ src/vdom/h.ts
@@ -19,13 +19,37 @@ export const VNodeTypes: Record<string, VNodeType> = {
   DYNAMIC: 2, // dynamic VNode (needs patch every time view changes)
 };
 
-export const h = (tag: string, children?: (VNode | string)[], props?: VNodeProps): VNode => {
+export const h = (selector: string, children?: (VNode | string)[], props?: VNodeProps): VNode => {
+  const tokens = selector.split(/(?=\.)|(?=#)|(?=\[)/);
+  const tag = tokens[0];
+  const attributes: Record<string, string> = {};
+  const directives: Record<string, string> = {};
+
+  if (tokens.length > 1) {
+    tokens.shift();
+
+    for (const token of tokens) {
+      switch (token[0]) {
+        case '#':
+          attributes.id = token.slice(1);
+          break;
+        case '.':
+          attributes.className += `${token.slice(1)} `;
+          break;
+        case '[':
+          const [key, value] = token.slice(1, -1).split('=');
+          attributes[key] = value;
+          break;
+      }
+    }
+  }
+
   return {
     tag,
     children: children || [],
     props: props || {
-      attributes: {},
-      directives: {},
+      attributes,
+      directives,
       ref: undefined,
       type: 0,
     },
