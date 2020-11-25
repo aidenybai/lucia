665cd7bbf501bbab415cc77c092d6ac29c6eb274
Aiden Bai <mail.contraband@gmail.com>
2020-11-25 15:58:32 -0800

    fix: mounting should occur after component registration

--- src/__test__/App.spec.ts
+++ src/__test__/App.spec.ts
@@ -15,8 +15,8 @@ describe('.App', () => {
     const fakeElem = document.createElement('div');
     const view = { test: 1 };
     const app = createApp(view);
+    app.component('Counter', '<div l-text="this.count"></div>');
     app.mount(fakeElem, true);
-    app.component('Counter', "<div l-text='this.count'></div>");
-    expect(app.components['COUNTER']).toBe("<div l-text='this.count'></div>");
+    expect(app.components['COUNTER']).toBe('<div l-text="this.count"></div>');
   });
 });
