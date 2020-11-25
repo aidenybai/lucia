94120d43d3e13759c90755ed3be5b0b67d5fc238
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-24 20:16:15 -0800

    fix: fix

--- src/directives/__test__/text.spec.ts
+++ src/directives/__test__/text.spec.ts
@@ -11,4 +11,14 @@ describe('.textDirective', () => {
     });
     expect(fakeElem.textContent).toBe('$0');
   });
+  it('should set the text content to the value', () => {
+    const fakeElem = document.createElement('div');
+    textDirective({
+      el: fakeElem,
+      name: `l-text`,
+      value: `count`,
+      view: {},
+    });
+    expect(fakeElem.textContent).toBe('count');
+  });
 });
--- src/directives/text.ts
+++ src/directives/text.ts
@@ -2,7 +2,17 @@ import compute from '../utils/compute';
 import { DirectiveArgs } from './IDirectiveArgs';
 
 export const textDirective = ({ el, value, view }: DirectiveArgs) => {
-  const out = compute(value, { $view: view, $el: el });
+  /*const out = compute(value, { $view: view, $el: el });
 
-  el.textContent = out ?? value;
+  el.textContent = out ?? value;*/
+
+  let out;
+
+  try {
+    out = compute(value, { $view: view, $el: el });
+  } catch {
+    out = value;
+  }
+
+  el.innerHTML = out;
 };
