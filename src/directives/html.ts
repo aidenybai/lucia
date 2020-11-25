e7c278ede079e48ce9b2a92b4be0161676262aa5
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-24 17:28:06 -0800

    fix(l-html): fix bug where it throws a reference error

--- src/directives/html.ts
+++ src/directives/html.ts
@@ -3,15 +3,18 @@ import { createApp } from '../index';
 import { DirectiveArgs } from './IDirectiveArgs';
 
 export const htmlDirective = ({ el, value, view }: DirectiveArgs) => {
-  const out = compute(value, { $view: view, $el: el });
 
   // Create shallow nested Lucia app
   const app = createApp({ ...view });
   app.mount(el, true);
 
-  if (out !== undefined) {
-    el.innerHTML = out;
-  } else {
-    el.innerHTML = value;
+  let out;
+
+  try {
+    out = compute(value, { $view: view, $el: el });
+  } catch {
+    out = value;
   }
+
+  el.innerHTML = out;
 };
