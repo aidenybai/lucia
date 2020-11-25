c03b6ba16c7e6c2a7f519f6615f16588f2925922
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-24 20:10:44 -0800

    test: tests for callbacks of events

--- src/directives/__test__/on.spec.ts
+++ src/directives/__test__/on.spec.ts
@@ -1,18 +1,21 @@
+import { fireEvent } from '@testing-library/dom';
+
 import { onDirective } from '../on';
 
 describe('.onDirective', () => {
   it('should attach onclick', () => {
     const fakeElem = document.createElement('button');
+    const mockCb = jest.fn();
     onDirective({
       el: fakeElem,
       name: 'l-on:click',
-      value: 'test()',
+      value: 'mockCb',
       view: {
-        test() {
-          return 0;
-        },
+        mockCb
       },
     });
     expect(typeof fakeElem.onclick).toBe('function');
+    fireEvent.click(fakeElem);
+    expect(mockCb.mock.calls[0]);
   });
 });
