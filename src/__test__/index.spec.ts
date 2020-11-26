ddf9f04f1c08d727b40d7c8fe7f724f980b578cf
Aiden Bai <mail.contraband@gmail.com>
2020-11-26 01:52:19 -0800

    test: cover use and init in index.ts

--- src/__test__/index.spec.ts
+++ src/__test__/index.spec.ts
@@ -1,4 +1,4 @@
-import { component } from '../index';
+import { component, use, init } from '../index';
 
 describe('.component', () => {
   it('should create it properly', () => {
@@ -7,5 +7,21 @@ describe('.component', () => {
       name: 'Counter',
       template: '<div l-text="this.count"></div>',
     });
+  });
+
+  it('register use correctly', () => {
+    const fakeElem = document.createElement('div');
+    fakeElem.setAttribute('l-use', 'App');
+    document.body.appendChild(fakeElem);
+
+    expect(use('App', {}));
+  });
+
+  it('init without error', () => {
+    const fakeElem = document.createElement('div');
+    fakeElem.setAttribute('l-init', '{ test: 1 }');
+    document.body.appendChild(fakeElem);
+
+    expect(init()).toEqual(undefined);
   });
 });
