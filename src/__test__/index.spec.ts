fa0246353c791bb5fced79ab12a1c4bfd8506fe5
Aiden Bai <mail.contraband@gmail.com>
2020-12-07 17:20:48 -0800

    test: add more init tests

--- src/__test__/index.spec.ts
+++ src/__test__/index.spec.ts
@@ -1,11 +1,40 @@
 import { init } from '../index';
 
 describe('.component', () => {
-  it('init without error', () => {
+  it('should init without error', () => {
+    document.body.innerHTML = '';
+
+    const fakeElem = document.createElement('div');
+    fakeElem.setAttribute('l-use', '{ test: 1 }');
+    document.body.appendChild(fakeElem);
+
+    expect(init().length).toEqual(1);
+  });
+  it('should not init automatic initiable elements', () => {
+    document.body.innerHTML = '';
+
     const fakeElem = document.createElement('div');
     fakeElem.setAttribute('l-init', '{ test: 1 }');
     document.body.appendChild(fakeElem);
 
-    expect(init()).toEqual(undefined);
+    expect(init().length).toEqual(0);
+  });
+  it('should not init if null', () => {
+    document.body.innerHTML = '';
+
+    const fakeElem = document.createElement('div');
+    fakeElem.setAttribute('l-use', 'null');
+    document.body.appendChild(fakeElem);
+
+    expect(init().length).toEqual(0);
+  });
+  it('should init custom directive', () => {
+    document.body.innerHTML = '';
+
+    const fakeElem = document.createElement('div');
+    fakeElem.setAttribute('l-customuse', '{ test: 1 }');
+    document.body.appendChild(fakeElem);
+
+    expect(init(document, 'customuse').length).toEqual(1);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Exports wrapped in Lucia namespace
 import { DIRECTIVE_PREFIX } from './defaults';
-import createApp from './App';
+import { App, createApp } from './App';
 
 import h from './vdom/h';
 import patch from './vdom/patch';
@@ -11,8 +11,12 @@ import { safeEval } from './vdom/utils/compute';
 
 export { createApp, h, compile, patch, observer, directives, renderDirective };
 
-export const init = (element: HTMLElement | Document = document, directive: string = 'use') => {
+export const init = (
+  element: HTMLElement | Document = document,
+  directive: string = 'use'
+): App[] => {
   const elements = [...element.querySelectorAll(`[${DIRECTIVE_PREFIX + directive}]`)];
+  const apps: App[] = [];
 
   elements.map((el) => {
     const view = el.getAttribute(DIRECTIVE_PREFIX + directive);
@@ -21,6 +25,9 @@ export const init = (element: HTMLElement | Document = document, directive: stri
     try {
       const app = createApp(safeEval(view));
       app.mount(el as HTMLElement);
+      apps.push(app);
     } catch (err) {}
   });
+
+  return apps;
 };
