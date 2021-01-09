bd381439eba744cc6415027228f9127c94a57f7a
Pulkit Ahuja <66655515+shadowtime2000@users.noreply.github.com>
2021-01-08 17:06:51 -0800

    fix: fix some stuff related to not binding funcs

--- src/vdom/utils/patterns.ts
+++ src/vdom/utils/patterns.ts
@@ -14,5 +14,5 @@ export const hasDirectiveRE = () => {
 export const expressionPropRE = (key: string, hasThis: boolean = true): RegExp => {
   // Utilizes \b (word boundary) for key differentiation.
   // Fails when next character is a \w (Word).
-  return new RegExp(`${hasThis ? 'this\\.' : ''}${key}\\b`, 'g');
+  return new RegExp(`${hasThis ? '\\$\\.' : ''}${key}\\b`, 'g');
 };
