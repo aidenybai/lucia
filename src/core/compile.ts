74830cfea5e225b491942aa8bfd0302b1866e897
Aiden Bai <mail.contraband@gmail.com>
2021-02-04 10:46:04 -0800

    feat: declare components inline

--- src/core/compile.ts
+++ src/core/compile.ts
@@ -6,13 +6,13 @@ import compute from './utils/computeExpression';
 
 export const removeDupesFromArray = (array: any[]): any[] => [...new Set(array)];
 
-export const isListRenderScope = (el: HTMLElement) => {
+export const isListRenderScope = (el: HTMLElement): boolean => {
   return el.hasAttribute(`${DIRECTIVE_PREFIX}for`);
 };
 
-export const isUnderListRenderScope = (el: HTMLElement) => {
+export const isUnderListRenderScope = (el: HTMLElement): boolean => {
   if (!el.parentElement) return false;
-  return el.parentElement!.hasAttribute(`${DIRECTIVE_PREFIX}for`);
+  return el.parentElement.hasAttribute(`${DIRECTIVE_PREFIX}for`);
 };
 
 export const createASTNode = (el: HTMLElement, state: State): ASTNode | null => {
@@ -20,28 +20,28 @@ export const createASTNode = (el: HTMLElement, state: State): ASTNode | null => 
 
   // Check if there are directives
   const hasDirectives = Object.keys(directives).length > 0;
+
   // Check if there are affected props in state
   const hasDepInDirectives = Object.values(directives).some(({ value }) =>
     Object.keys(state).some((prop) => expressionPropRE(prop).test(value))
   );
+  const type = hasDepInDirectives ? 1 : 0;
 
   if (!hasDirectives) return null;
-  return {
-    el,
-    deps: deps as string[],
-    directives: directives as DirectiveKV,
-    type: hasDepInDirectives ? 1 : 0,
-  };
+
+  return { el, deps, directives, type };
 };
 
 export const collectAndInitDirectives = (
   el: HTMLElement,
   state: State = {}
-): (DirectiveKV | string[])[] => {
+): [DirectiveKV, string[]] => {
   const directives: DirectiveKV = {};
   const nodeDeps = [];
 
   for (const { name, value } of el.attributes) {
+    if (name === `${DIRECTIVE_PREFIX}state`) continue;
+
     const depsInFunctions: string[] = [];
     const propsInState: string[] = Object.keys(state);
     let returnable = true;
@@ -50,6 +50,7 @@ export const collectAndInitDirectives = (
     const deps: string[] = propsInState.filter((prop) => {
       const hasDep = expressionPropRE(prop).test(String(value));
 
+      // Compare toString value of function
       if (typeof state[prop] === 'function' && hasDep) {
         const depsInFunction = propsInState.filter((p) =>
           expressionPropRE(p).test(String(state[prop]))
@@ -88,7 +89,7 @@ export const collectAndInitDirectives = (
 export const extractNodeChildrenAsCollection = (
   rootNode: HTMLElement,
   isListGroup: boolean = false
-) => {
+): HTMLElement[] => {
   const collection: HTMLElement[] = [];
   const isList = isListRenderScope(rootNode);
   const isUnderList = isUnderListRenderScope(rootNode);
@@ -99,15 +100,16 @@ export const extractNodeChildrenAsCollection = (
   if (!isListGroup || !isList) collection.push(rootNode);
 
   // Is not a list or under a list, but pass if is a list group
-  if ((!isList && !isUnderList) || isListGroup) {
+  if (isListGroup || (!isList && !isUnderList)) {
     for (const childNode of rootNode.childNodes) {
       if (childNode.nodeType === Node.ELEMENT_NODE) {
         if (!isListGroup && isListRenderScope(childNode as HTMLElement)) {
           // Push root if it is a list render (don't want to push unrendered template)
           collection.push(childNode as HTMLElement);
         } else {
-          // Push all children into array (recursive flattening)
+          // Skip over nested components (independent compile request)
           if ((childNode as HTMLElement).hasAttribute(`${DIRECTIVE_PREFIX}state`)) continue;
+          // Push all children into array (recursive flattening)
           collection.push(
             ...extractNodeChildrenAsCollection(childNode as HTMLElement, isListGroup)
           );
@@ -127,7 +129,6 @@ export const compile = (el: HTMLElement, state: State = {}): ASTNode[] => {
   const nodes: HTMLElement[] = extractNodeChildrenAsCollection(el, isListGroup);
 
   for (const node of nodes) {
-    if (node.hasAttribute(`${DIRECTIVE_PREFIX}state`)) continue;
     if (hasDirectiveRE().test(node.outerHTML)) {
       // Creates AST Node from real DOM nodes
       const newASTNode = createASTNode(node, state);
