b8e08c82a10c347190cb9cfa217a11238345ef8b
Aiden Bai <mail.contraband@gmail.com>
2021-03-02 16:56:35 -0800

    fix: in keyword throwing error

--- src/browser.ts
+++ src/browser.ts
@@ -3,7 +3,7 @@ import * as Lucia from './index';
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', () => {
     // @ts-expect-error
-    if (customLuciaInit in window) {
+    if (window.customLuciaInit) {
       // @ts-expect-error
       window.customLuciaInit(Lucia.init);
     } else {
