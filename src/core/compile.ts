13bfe9aa18cccce7f494f47f9df97864fae81b8a
Aiden Bai <mail.contraband@gmail.com>
2021-04-15 11:22:45 -0700

    fix: double assertion to ensure boolean value

--- src/core/compile.ts
+++ src/core/compile.ts
@@ -10,7 +10,7 @@ export const isListRenderScope = (el: HTMLElement): boolean => {
 };
 
 export const isUnderListRenderScope = (el: HTMLElement): boolean => {
-  return el.parentElement && el.parentElement.hasAttribute(`${DIRECTIVE_PREFIX}for`);
+  return !!el.parentElement && el.parentElement.hasAttribute(`${DIRECTIVE_PREFIX}for`);
 };
 
 export const createASTNode = (el: HTMLElement, state: State): ASTNode | undefined => {
