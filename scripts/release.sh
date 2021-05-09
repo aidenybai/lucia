ec929a112a7ebfd7dacb27186fd079a876a16201
Aiden Bai <mail.contraband@gmail.com>
2021-05-09 00:43:01 -0700

    chore: add error fallbacks for release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 YELLOW_COLOR="\u001b[33m"
 GREEN_COLOR="\033[0;32m"
+RED_COLOR='\033[0;31m'
+BLUE_COLOR='\033[0;34m'
 RESET_COLOR="\u001b[39m"
 SPACER="                   "
 
-printf "${YELLOW_COLOR}Checking for errors...${RESET_COLOR}"
-source scripts/check.sh &>/dev/null || exit 1
-printf "\r${GREEN_COLOR}No errors found.${RESET_COLOR}${SPACER}\n"
-printf "${YELLOW_COLOR}Building distribution bundles...${RESET_COLOR}"
-source scripts/build.sh &>/dev/null || exit 1
-printf "\r${GREEN_COLOR}Built distribution bundles.${SPACER}${RESET_COLOR}\n${YELLOW_COLOR}Run \`np\` to publish.${RESET_COLOR}"
+printf "${YELLOW_COLOR}⚠ Checking for errors...${RESET_COLOR}"
+source scripts/check.sh &>/dev/null || (printf "\r${RED_COLOR}✗ Error during checks. Run \`sh scripts/check.sh\`.${RESET_COLOR}\n" && exit 1)
+printf "\r${GREEN_COLOR}✓ No errors found.${RESET_COLOR}${SPACER}\n"
+printf "${YELLOW_COLOR}⚠ Building distribution bundles...${RESET_COLOR}"
+source scripts/build.sh &>/dev/null || (printf "\r${RED_COLOR}✗ Error during build. Run \`sh scripts/build.sh\`.${RESET_COLOR}\n" && exit 1)
+printf "\r${GREEN_COLOR}✓ Built distribution bundles.${SPACER}${RESET_COLOR}\n${BLUE_COLOR}ⓘ Run \`np\` to publish.${RESET_COLOR}\n"
--- scripts/yarn.sh
+++ scripts/yarn.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-YELLOW_COLOR="\u001b[33m"
+RED_COLOR='\033[0;31m'
 RESET_COLOR="\u001b[39m"
 
 if [[ $npm_execpath != *"yarn"* ]]; then
-  printf "${YELLOW_COLOR}This repository requires Yarn 1.x for scripts to work properly.${RESET_COLOR}\n\n"
+  printf "${RED_COLOR}✗ Lucia requires Yarn 1.x for scripts to work properly.${RESET_COLOR}\n\n"
   exit 1
 fi
