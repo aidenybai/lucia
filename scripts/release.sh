49a6dba7e5ebec018f8e7aacabe420f94a37a48a
Aiden Bai <mail.contraband@gmail.com>
2021-05-08 18:58:33 -0700

    chore: custom logs for pre-commit hook and release

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,11 @@
 YELLOW_COLOR="\u001b[33m"
 GREEN_COLOR="\033[0;32m"
 RESET_COLOR="\u001b[39m"
+SPACER="                   "
 
-printf "${YELLOW_COLOR}Checking for errors...${RESET_COLOR}\n"
-source scripts/check.sh
-printf "${GREEN_COLOR}No errors found.${RESET_COLOR}\n"
-printf "${YELLOW_COLOR}Building distribution bundles...${RESET_COLOR}\n"
-source scripts/build.sh
-printf "${GREEN_COLOR}Built distribution bundles.${RESET_COLOR}\n"
-printf "${YELLOW_COLOR}Please run \`np\` manually to publish the package.${RESET_COLOR}"
+printf "${YELLOW_COLOR}Checking for errors...${RESET_COLOR}"
+source scripts/check.sh &>/dev/null || exit 1
+printf "\r${GREEN_COLOR}No errors found.${RESET_COLOR}${SPACER}\n"
+printf "${YELLOW_COLOR}Building distribution bundles...${RESET_COLOR}"
+source scripts/build.sh &>/dev/null || exit 1
+printf "\r${GREEN_COLOR}Built distribution bundles.${SPACER}${RESET_COLOR}\n${YELLOW_COLOR}Run \`np\` to publish.${RESET_COLOR}"
