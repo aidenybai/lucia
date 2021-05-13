a95426d639fe46efa14a5b2cf277bc8121f48c84
Aiden Bai <mail.contraband@gmail.com>
2021-05-12 23:54:39 -0700

    fix: remove preinstall hook

--- scripts/yarn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-RED_COLOR='\033[0;31m'
-RESET_COLOR="\u001b[39m"
-
-if [[ $npm_execpath != *"yarn"* ]]; then
-  printf "${RED_COLOR}✗ Lucia requires Yarn 1.x for scripts to work properly.${RESET_COLOR}\n\n"
-  exit 1
-fi
